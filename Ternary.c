1be206621ab5928231e0698dd3e8b4e280f7af44
psarmiento <2010sarmiento@gmail.com>
2019-02-14 18:24:52 -0800

    Create Ternary.c

---
+++ Ternary.c
@@ -0,0 +1,13 @@
+// Ternary Operator - Can use in place of if/else or switch
+#include<stdio.h>
+
+int main(){
+    int a, b;
+
+    a = 10;
+    printf("Value of b is %d\n", (a ==1) ? 20:30);
+    printf("Value of b is %d\n", a ==10 ? 20:30);
+
+
+    return 0;
+}
