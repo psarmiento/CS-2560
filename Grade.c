8d00fbb7ddd50a4048234fa351c1871c04395577
psarmiento <2010sarmiento@gmail.com>
2019-02-18 13:42:12 -0800

    Create Grade.c

---
+++ Grade.c
@@ -0,0 +1,30 @@
+// Switch -- Can use in place of if/else or turnery
+
+#include <stdio.h>
+
+int main() {
+  // locla variable definition 
+  char grade = 'B';
+  
+  switch(grade){
+    case 'A':
+      printf("Excellent!\n");
+      break;
+    case 'B':     // Nothing happens
+    case 'C':
+      printf("Well done \n");
+      break;
+    case 'D':
+      printf("You passed \n");
+      break;
+    case 'F':
+      printf("Better try again\n");
+      break;
+    default:
+      printf("Invalid grade\n");
+  }
+
+
+printf("Your grade is %c\n", grade);
+return 0;
+}
