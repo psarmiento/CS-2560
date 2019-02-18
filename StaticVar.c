fdecc59c4ded1251f8263199baa5db04466179da
psarmiento <2010sarmiento@gmail.com>
2019-02-18 13:45:43 -0800

    Create StaticVar.c

---
+++ StaticVar.c
@@ -0,0 +1,24 @@
+// Global variables
+int A;
+int B;
+
+int fun(){
+  static int count = 0;   // Static variable 
+  count++;
+  return count;
+}
+
+int Add(){
+  return A + B;
+}
+
+int main(){
+  int answer;   // Local variable 
+  A = 5; 
+  B = 7;
+  answer = Add();
+  printf("%d\n", answer);
+  printf("%d\n", fun());
+  printf("%d\n", fun());
+  return 0;
+}
