8b70a2a69c1da5ace09139e1c40e834d1df89e80
psarmiento <2010sarmiento@gmail.com>
2019-02-19 12:02:18 -0800

    Update Union.c

--- Union.c
+++ Union.c
@@ -1,22 +1,23 @@
 //UNION
 //only one data type can exist at any given time in a union
 //The size allocated for a union will be the largest type.
- 
+
 #include <stdio.h>
 #include <string.h>
- 
+
 //size allocated is 20 bytes. Regardless of which data you use. This way computer can change it at runtime
 union Data {
    int i;  //4 bytes
    float f; //4 bytes
    char str[20];  //20 bytes
 };
- 
+
 int main( ) {
- 
-   union Data data;        
- 
-   printf( "Memory size occupied by data : %d\n", sizeof(data));
- 
+
+   union Data data;
+
+   int size = sizeof(data);
+   printf( "Memory size occupied by data : %d\n", size);
+
    return 0;
 }
