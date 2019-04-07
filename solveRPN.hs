01904364fd33515d4477d8c4e1cffbe21d4de807
psarmiento <2010sarmiento@gmail.com>
2019-04-06 18:57:47 -0700

    Create solveRPN.hs

---
+++ solveRPN.hs
@@ -0,0 +1,6 @@
+solveRPN :: (Num a, Read a) => String -> a  
+solveRPN = head . foldl foldingFunction [] . words  
+    where   foldingFunction (x:y:ys) "*" = (x * y):ys  
+            foldingFunction (x:y:ys) "+" = (x + y):ys  
+            foldingFunction (x:y:ys) "-" = (y - x):ys  
+            foldingFunction xs numberString = read numberString:xs
